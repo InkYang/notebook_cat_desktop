b9097c02797c9585a938619f2fb7b81a7aaca209
Yang <inkyang@users.noreply.github.com>
2018-07-18 22:42:46 +0800

    字典的添加

--- cat.py
+++ cat.py
@@ -1,6 +1,10 @@
 import json
 import sys
 
+if len(sys.argv) < 2:
+    raise Exception('参数数量必须大于1！')
+
+
 # accept argument lst
 notebook_path_lst = sys.argv[1:]
 
@@ -8,25 +12,17 @@ target_notebook = {}
 cells_lst = []
 # read notebook path list
 for path in notebook_path_lst:
-    notebook = open(path)
-    notebook_str = notebook.read()
-    notebook_json = json.loads(notebook_str)
-    cells = notebook_json['cells']
-    cells_lst += cells
+    with open(path) as notebook:
+        notebook_str = notebook.read()
+        notebook_json = json.loads(notebook_str)
+        cells = notebook_json['cells']
+        cells_lst += cells
 
 target_notebook['cells'] = cells_lst
 
+# 转化成字符串
+target_notebook_str = json.dumps(target_notebook)
 
-# cat two notebooks
-target_cells = cells1 + cells2
-
-target_notebook = {}
-
-target_notebook['cells'] = target_cells
-target_notebook.update(notebook1_json)
-
-target_str = json.dumps(target_notebook)
-
-
-target = open('target_notebook.ipynb','w')
-target.write(target_str)
+# 存入文件
+with open('target_notebook.ipynb','w') as target:
+    target.write(target_notebook_str)
