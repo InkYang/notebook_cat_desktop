641059221ed341f78ab8144d78966e1f38afdf59
Yang <inkyang@users.noreply.github.com>
2018-07-18 21:19:46 +0800

    two notebooks cat

--- cat.py
+++ cat.py
@@ -6,21 +6,26 @@ notebook1_str = notebook1.read()
 
 notebook1_json = json.loads(notebook1_str)
 
-
-
 cells1 = notebook1_json['cells']
 
 notebook2 = open('2.ipynb')
 notebook2_str = notebook2.read()
 notebook2_json = json.loads(notebook2_str)
 
-
 cells2 = notebook2_json['cells']
+
+del notebook1_json['cells']
 
 # cat two notebooks
 target_cells = cells1 + cells2
 
-target_json = json.dumps(target_cells)
+target_notebook = {}
+
+target_notebook['cells'] = target_cells
+target_notebook.update(notebook1_json)
+
+target_str = json.dumps(target_notebook)
+
 
 target = open('target_notebook.ipynb','w')
-target.write(target_json)
+target.write(target_str)
